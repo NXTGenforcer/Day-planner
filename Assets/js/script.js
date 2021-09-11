e3caef28a0e584918538119e4f4a49f242eee53f
Genforcer <gskywalker11@gmail.com>
2021-09-11 11:33:29 -0400

    finished project

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,42 @@
 var tableEl = $(".table");
 var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-var btn = $(".saveBtn");
-var textArea = $(".textarea");
-
+var currentTime = moment().hour();
 $(document).ready(function () {
-  for (let i = 0; i < times.length; i++) {
-    var element = times[i];
-    console.log(element);
-    tableEl.append(
-      $(
-        `<tr>
-          <th scope="row">${element}</th>
-             <td><textarea class="textarea" name="textarea" id="textarea" cols="100" rows="1"></textarea></td>
-             <td><button class="saveBtn">Save</button></td></tr>`
-      )
-    );
+  function init() {
+    for (let i = 0; i < times.length; i++) {
+      var element = times[i];
+      var elementTime = parseInt(element.slice(0, -2));
+      var classString = "";
+      var storage = localStorage.getItem(element);
+      var textAreaString = "";
+      if (elementTime < 9) {
+        elementTime = elementTime + 12;
+      }
+      if (elementTime < currentTime) {
+        classString = "past";
+      } else if (elementTime === currentTime) {
+        classString = "present";
+      } else {
+        classString = "future";
+      }
+      if (storage) {
+        textAreaString = storage;
+      }
+      tableEl.append(
+        $(
+          `<tr>
+          <th scope="row" >${element}</th>
+            <td><textarea class="textbox ${classString}" name="textarea" id="${element}" cols="100" rows="1">${textAreaString}</textarea></td>
+            <td><button class="far fa-save saveBtn" id="${element}"></button></td></tr>`
+        )
+      );
+    }
   }
-});
-
-btn.addEventLisenter(function () {
-  textArea.text.value.json();
+  init();
+  $(".fa-save").on("click", function () {
+    var id = $(this).attr("id");
+    var taskText = $(this).parent().siblings("td").children(".textbox").val();
+    localStorage.setItem(id, taskText);
+    init();
+  });
 });
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Day-planner
 
+<h1>Welcome to your day planner!
+
+<h3>This was created to help organize your everyday 9-5 through a url. We met some struggles through this process as you can see the task area colors change as the time of day does. We hope you can make use of this planner on a regular basis. Thank you for visiting our webpage!
+
 ![Work Day Scheduler - Google Chrome 9_11_2021 11_22_45 AM](https://user-images.githubusercontent.com/84356164/132952970-349125f5-9a68-4eeb-8d38-2219087de423.png)
+
+Active url: https://nxtgenforcer.github.io/Day-planner/
+Repository url:https://github.com/NXTGenforcer/Day-planner
